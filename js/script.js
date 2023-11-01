4fc7b852f706eb1bd5e2d357a2bec493b58fc80c
Valter <babik.valti@gmail.com>
2023-11-01 22:21:03 +0100

    Next element, tile update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 import { AppState } from "./state.js";
-import {render} from "./renderer.js";
+import { render as gameRender } from "./gameRenderer.js";
+import { render  as prevRender } from "./previewRenderer.js";
 
 const state = new AppState()
 const game= document.querySelector("#game")
+const nextElem = document.querySelector("#nextElem")
 
 state.init()
-game.innerHTML += render(state)
+game.innerHTML += gameRender(state)
+nextElem.innerHTML += state.nextElem.time
+nextElem.innerHTML += prevRender(state)
--- js/state.js
+++ js/state.js
@@ -2,8 +2,11 @@ import {Field, FieldType} from "./field.js";
 
 export class AppState {
     board = []
+    nextElem = []
+    preview = []
 
     init() {
+        //Board
         this.board = []
         for (let i = 0; i < 11; i++) {
             this.board[i] = []
@@ -16,5 +19,182 @@ export class AppState {
         this.board[5][3].type = FieldType.HILL
         this.board[8][9].type = FieldType.HILL
         this.board[9][5].type = FieldType.HILL
+
+        //Next element
+        let r = Math.floor(Math.random() * elements.length)
+        this.nextElem = elements[r]
+
+
+        for (let i = 0; i < 3; i++) {
+            this.preview[i] = []
+            for (let j = 0; j < 3; j++) {
+                this.preview[i][j] = new Field()
+                if (this.nextElem.shape[i][j] === 1) {
+                    switch (this.nextElem.type) {
+                        case 'town':
+                            this.preview[i][j].type = FieldType.VILLAGE
+                            break
+                        case 'water':
+                            this.preview[i][j].type = FieldType.WATER
+                            break
+                        case 'forest':
+                            this.preview[i][j].type = FieldType.FOREST
+                            break
+                        case 'farm':
+                            this.preview[i][j].type = FieldType.FARM
+                            break
+                    }
+                } else {
+                    this.preview[i][j].type = FieldType.EMPTY
+                }
+            }
+        }
     }
 }
+
+const elements = [
+    {
+        time: 2,
+        type: 'water',
+        shape: [[1,1,1],
+            [0,0,0],
+            [0,0,0]],
+        rotation: 0,
+        mirrored: false
+    },
+    {
+        time: 2,
+        type: 'town',
+        shape: [[1,1,1],
+            [0,0,0],
+            [0,0,0]],
+        rotation: 0,
+        mirrored: false
+    },
+    {
+        time: 1,
+        type: 'forest',
+        shape: [[1,1,0],
+            [0,1,1],
+            [0,0,0]],
+        rotation: 0,
+        mirrored: false
+    },
+    {
+        time: 2,
+        type: 'farm',
+        shape: [[1,1,1],
+            [0,0,1],
+            [0,0,0]],
+        rotation: 0,
+        mirrored: false
+    },
+    {
+        time: 2,
+        type: 'forest',
+        shape: [[1,1,1],
+            [0,0,1],
+            [0,0,0]],
+        rotation: 0,
+        mirrored: false
+    },
+    {
+        time: 2,
+        type: 'town',
+        shape: [[1,1,1],
+            [0,1,0],
+            [0,0,0]],
+        rotation: 0,
+        mirrored: false
+    },
+    {
+        time: 2,
+        type: 'farm',
+        shape: [[1,1,1],
+            [0,1,0],
+            [0,0,0]],
+        rotation: 0,
+        mirrored: false
+    },
+    {
+        time: 1,
+        type: 'town',
+        shape: [[1,1,0],
+            [1,0,0],
+            [0,0,0]],
+        rotation: 0,
+        mirrored: false
+    },
+    {
+        time: 1,
+        type: 'town',
+        shape: [[1,1,1],
+            [1,1,0],
+            [0,0,0]],
+        rotation: 0,
+        mirrored: false
+    },
+    {
+        time: 1,
+        type: 'farm',
+        shape: [[1,1,0],
+            [0,1,1],
+            [0,0,0]],
+        rotation: 0,
+        mirrored: false
+    },
+    {
+        time: 1,
+        type: 'farm',
+        shape: [[0,1,0],
+            [1,1,1],
+            [0,1,0]],
+        rotation: 0,
+        mirrored: false
+    },
+    {
+        time: 2,
+        type: 'water',
+        shape: [[1,1,1],
+            [1,0,0],
+            [1,0,0]],
+        rotation: 0,
+        mirrored: false
+    },
+    {
+        time: 2,
+        type: 'water',
+        shape: [[1,0,0],
+            [1,1,1],
+            [1,0,0]],
+        rotation: 0,
+        mirrored: false
+    },
+    {
+        time: 2,
+        type: 'forest',
+        shape: [[1,1,0],
+            [0,1,1],
+            [0,0,1]],
+        rotation: 0,
+        mirrored: false
+    },
+    {
+        time: 2,
+        type: 'forest',
+        shape: [[1,1,0],
+            [0,1,1],
+            [0,0,0]],
+        rotation: 0,
+        mirrored: false
+    },
+    {
+        time: 2,
+        type: 'water',
+        shape: [[1,1,0],
+            [1,1,0],
+            [0,0,0]],
+        rotation: 0,
+        mirrored: false
+    },
+]
